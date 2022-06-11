01e1d0ddae8c1d6b75c6f0c34e2ce0873ce28483
ws8313 <ws8313@naver.com>
2022-06-12 01:33:20 +0900

    feat: intro animation Slider

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useEffect, useState } from "react";
 import Main from "./components/Main";
 import Intro from "./components/Intro";
 import Information from "./components/Information";
 import Project from "./components/Project";
+import Slider from "./components/Slider";
 
 import { flexCenter } from "./styles/theme";
 import styled from "styled-components";
@@ -14,14 +16,28 @@ const AppContainer = styled.div`
 `;
 
 function App() {
+  const [slider, setSlider] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setSlider(false);
+    }, 2500);
+  }, []);
+
   return (
     <AppContainer>
       <div className="App">
         <div className="wrapper">
-          <Main />
-          <Intro />
-          <Information />
-          <Project />
+          {slider ? (
+            <Slider />
+          ) : (
+            <>
+              <Main />
+              <Intro />
+              <Information />
+              <Project />
+            </>
+          )}
         </div>
       </div>
     </AppContainer>
